458edc87c9f77c3ba56d9956f5042615c43bee63
salimbene <39461326+salimbene@users.noreply.github.com>
2018-05-20 18:37:19 -0300

    Create ej32.py

---
+++ ej32.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python2.7
+"""
+32. Crear un array de 20 elementos con nombres de personas. Visualizar los elementos de la lista.
+"""
+
+n = ['Jorge','Carlos','Pedro','Matias','Martin','Vicky','Eugenia','Maria','Cecilia','Carolina','Dario','Leonardo','Natalia','Jose','Pablo','Juliana','Fernanda','Marina','Damian','Guido']
+
+for x in n:
+	print(x)
