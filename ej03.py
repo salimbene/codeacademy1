8dd1d10885fa917259c71eccc6ea7e77818706d9
salimbene <39461326+salimbene@users.noreply.github.com>
2018-05-20 17:26:35 -0300

    Create ej03.py

---
+++ ej03.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python2.7
+
+"""
+3. Hacer un pseudocodigo que lea 3 numeros y muestre el promedio entre ellos.
+"""
+
+a = float(raw_input("Ingrese nro1: "))
+b = float(raw_input("Ingrese nro1: "))
+c = float(raw_input("Ingrese nro1: "))
+print "El promedio es: ",(a+b+c)/3
