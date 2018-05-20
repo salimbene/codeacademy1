9de82b6a12311b60525940eaf18fbb944ec0b387
salimbene <39461326+salimbene@users.noreply.github.com>
2018-05-20 17:33:41 -0300

    Create ej15.py

---
+++ ej15.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python2.7
+
+"""
+15. Introducir un numero por teclado y que el algoritmo nos diga si es par o impar.
+"""
+
+n = int(raw_input("Ingrese nro:"))
+if n % 2 == 0:
+	print "PAR"
+else:
+	print "INPAR"
