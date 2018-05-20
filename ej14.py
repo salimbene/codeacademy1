0d4b40abb30407cb3ab2049da539faeef56def3b
salimbene <39461326+salimbene@users.noreply.github.com>
2018-05-20 17:33:17 -0300

    Create ej14.py

---
+++ ej14.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python2.7
+
+"""
+14. Introducir un numero por teclado y que el algoritmo nos diga si es positivo o negativo.
+"""
+
+n=int(raw_input("Ingrese nro:"))
+if n > 0:
+	print "Es positivo"
+else:
+	print "Es negativo"
