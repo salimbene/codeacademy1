9dd9f4e5dc56c2a19425266682055d1843437477
salimbene <39461326+salimbene@users.noreply.github.com>
2018-05-20 17:28:47 -0300

    Create ej05.py

---
+++ ej05.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python2.7
+
+"""
+5. Hacer un pseudocodigo que lea dos numeros y muestre el cuadrado de ambos.
+"""
+
+a = int(raw_input("Ingrese nro1: "))
+b = int(raw_input("Ingrese nro2: "))
+print "El cuadrado de",a,"es ",a*a
+print "El cuadrado de",b,"es ",b*b
