bcca4a450ab4a9c130c1f6467edc19732bfa6371
salimbene <39461326+salimbene@users.noreply.github.com>
2018-05-20 17:32:52 -0300

    Create ej13.py

---
+++ ej13.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python2.7
+
+"""
+13. Hacer un pseudocodigo que solo nos permita introducir S o N.
+"""
+
+n=""
+while (n != 'S') and (n != 'N'):
+	n = raw_input('Ingrese N o S')
+
+print "Ok"
