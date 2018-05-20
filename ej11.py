ce1c87927df0532fff9d527ee38ab6c4bbd8fbf5
salimbene <39461326+salimbene@users.noreply.github.com>
2018-05-20 17:31:58 -0300

    Create ej11.py

---
+++ ej11.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python2.7
+
+"""
+11. Hacer un pseudocodigo que imprima todos los numeros naturales que hay desde la unidad hasta
+un numero que introducimos por teclado.
+"""
+
+a=1
+n = int(raw_input('Ingrese un nro:'))
+while (a <= n):
+	print a
+	a+=1
